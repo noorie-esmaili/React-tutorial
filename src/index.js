8d6ce47fb4d6e5fc131fea1efccffafc7da15c6b
Noorie Esmaili <ne222hz@student.lnu.se>
2021-05-17 12:58:43 +0200

    Mini book project
    Signed-off-by: Noorie Esmaili <ne222hz@student.lnu.se>

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// JSX Rules
-// return single element
-// div / section / article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element 
-// formatting 
+// Nested Components, React Tools
 
-function Greeting() {
+function BookList() {
     return (
-    <div>
-        <h3>hello people</h3>
-        <ul>
-            <li>
-                <a href="#">hello world
-                </a>
-            </li>
-            <img src='' alt='' />
-            <input type='text' name='' id='' />
-        </ul>
-    </div>
+        <section>
+            <Book />
+        </section>
     )
 }
 
-// const Greeting = () => {
-//     return React.createElement('h1', {}, React.createElement(
-//         'div',
-//         {},
-//         React.createElement('h1', {}, 'hello world')
-//         ));
-// };
+const Book = () => {
+    return <article>
+        <Image></Image>
+        <Title/>
+        <Author/>
+    </article>
+}
+
+const Image = () => {
+    return <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+    alt="" />
+}
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+
+const Author = () => <h4>Amelia Hepworth</h4>
 
 ReactDom.render(
-<Greeting/>, document.getElementById('root')
+<BookList/>, document.getElementById('root')
 );
