6acfd8de31c2786e2d3033b19f33095f3d117bbb
Noorie Esmaili <ne222hz@student.lnu.se>
2021-06-09 19:17:10 +0200

    CSS
    Signed-off-by: Noorie Esmaili <ne222hz@student.lnu.se>

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
-// Nested Components, React Tools
+// CSS
+import "./index.css";
 
 function BookList() {
-    return (
-        <section>
-            <Book />
-        </section>
-    )
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
 }
 
 const Book = () => {
-    return <article>
-        <Image></Image>
-        <Title/>
-        <Author/>
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title />
+      <Author />
     </article>
-}
+  );
+};
 
 const Image = () => {
-    return <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-    alt="" />
-}
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
 
 const Title = () => <h1>I Love You to the Moon and Back</h1>;
 
-const Author = () => <h4>Amelia Hepworth</h4>
-
-ReactDom.render(
-<BookList/>, document.getElementById('root')
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Amelia Hepworth
+  </h4>
 );
+
+ReactDom.render(<BookList />, document.getElementById("root"));
