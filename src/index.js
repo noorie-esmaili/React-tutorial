8faaa7a6abe1f472a09aa70860d23dcc4a65e336
Noorie Esmaili <ne222hz@student.lnu.se>
2021-06-10 09:27:02 +0200

    Props
    Signed-off-by: Noorie Esmaili <ne222hz@student.lnu.se>

--- src/index.js
+++ src/index.js
@@ -4,39 +4,48 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
+// setup vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg",
+  title: "Our Class is a Family",
+  author: "Shannon Olsen",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   );
 };
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Amelia Hepworth
-  </h4>
-);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
