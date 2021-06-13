4e3cef6c224550d1abaa4bfdab46b3e3ea405eef
Noorie Esmaili <ne222hz@student.lnu.se>
2021-06-13 12:45:21 +0200

    Props
    Signed-off-by: Noorie Esmaili <ne222hz@student.lnu.se>

--- src/index.js
+++ src/index.js
@@ -24,7 +24,12 @@ function BookList() {
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla quos
+          dignissimos minima aut iusto, esse commodi enim sunt voluptates rem?
+        </p>
+      </Book>
       <Book
         img={secondBook.img}
         title={secondBook.title}
@@ -34,13 +39,14 @@ function BookList() {
   );
 }
 
-const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
+const Book = (props) => {
+  const { img, title, author, children } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };
